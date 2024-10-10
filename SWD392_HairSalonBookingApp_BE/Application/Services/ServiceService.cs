a4f9f23160af60da88b6d97752855848e4aa5961
Khiem <aidolamgidi@gmail.com>
2024-10-11 03:11:26 +0700

    add Service service, ComboService service

---
+++ SWD392_HairSalonBookingApp_BE/Application/Services/ServiceService.cs
@@ -0,0 +1,14 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+    public class ServiceService : IServiceService
+    {
+    }
+}
