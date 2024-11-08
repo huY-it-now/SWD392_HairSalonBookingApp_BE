794c562e3f8208ba15cf0187be8ecd48f0aad2fe
TamTam <Tamtp93@gmail.com>
2024-11-08 10:28:04 +0700

    add IAuthService

---
+++ SWD392_HairSalonBookingApp_BE/Application/Interfaces/IAuthService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Contracts.DTO.Auth;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Application.Interfaces
+{
+    public interface IAuthService
+    {
+        public Task<GoogleAuthResponses> HandleGoogleLogin(string rdc, GgAuthInfo info);
+        public Task<IActionResult> HandleRefresh(RefreshReq req);
+        public Task<IActionResult> HandleLogout(LogoutReq req);
+    }
+}
